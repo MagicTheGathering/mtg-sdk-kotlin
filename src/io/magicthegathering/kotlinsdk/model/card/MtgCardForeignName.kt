758c08b7d8981420ee46cb2b461d42549c186674
Joshua Lamson <joshua.lamson@possible.com>
2018-09-02 09:41:31 -0600

    Make all models data classes

--- src/io/magicthegathering/kotlinsdk/model/card/MtgCardForeignName.kt
+++ src/io/magicthegathering/kotlinsdk/model/card/MtgCardForeignName.kt
@@ -1,3 +1,8 @@
 package io.magicthegathering.kotlinsdk.model.card
 
-class MtgCardForeignName(val name: String, val imageUrl: String?, val language: String, val multiverseid: Int?)
+data class MtgCardForeignName(
+        val name: String,
+        val imageUrl: String?,
+        val language: String,
+        val multiverseid: Int?
+)
